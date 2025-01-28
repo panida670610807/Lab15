241e19cd74cabfaea2e5447c092fba871041150b
panida670610807 <hiyik757.gmail>
2025-01-28 15:10:49 +0700

    y

--- lab15_3.cpp
+++ lab15_3.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main(){
-	int a[] = {1,2,4,6,9}; 
-	double b[] = {5.5,2.1,13,-7,6.9,0,25,11.1,-4}; 
-	
-	pa = max_element(); 
-	cout << "Address of the highest value in array A is " << pa << "\n";
-	cout << "The highest value in array A is "<< pa << "\n";
-	
-	pb = max_element(); 
-	cout << "Address of the highest value in array B is " << pb << "\n";
-	cout << "The highest value in array B is "<< pb << "\n";
+    int data[] = {7,8,2,1,2,4,6,9,0,1}; 
 
-	return 0;
+    cout << "Before: ";
+    for(int i = 0; i < 10; i++) cout << data[i] << " ";
+    
+    sort(data, data + 5);
+    sort(data + 5, data + 10);
+    
+    cout << "\nAfter: ";
+    for(int i = 0; i < 10; i++) cout << data[i] << " ";
+    
+    return 0;
 }
-
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int *,int *,int *,int *);
+void shuffle(int &,int &,int &,int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,12 +11,32 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(&a,&b,&c,&d);
+	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << endl;
 	}
 	
 	return 0;
 }
+void shuffle(int &a, int &b, int &c, int &d) {
+    int random1 = rand() % 4;
+    int random2 = rand() % 4;
+    
+    while (random1 == random2) {
+        random2 = rand() % 4;
+    }
 
-//Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+    if (random1 == 0 && random2 == 1) swap(a, b);
+    else if (random1 == 0 && random2 == 2) swap(a, c);
+    else if (random1 == 0 && random2 == 3) swap(a, d);
+    else if (random1 == 1 && random2 == 2) swap(b, c);
+    else if (random1 == 1 && random2 == 3) swap(b, d);
+    else if (random1 == 2 && random2 == 3) swap(c, d);
+    else if (random1 == 1 && random2 == 0) swap(b, a);
+    else if (random1 == 2 && random2 == 0) swap(c, a);
+    else if (random1 == 3 && random2 == 0) swap(d, a);
+    else if (random1 == 2 && random2 == 1) swap(c, b);
+    else if (random1 == 3 && random2 == 1) swap(d, b);
+    else if (random1 == 3 && random2 == 2) swap(d, c);
+}
+
+//Do not modify source code above this line
